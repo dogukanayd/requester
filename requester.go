4eb4c7fbc159cd43db427a8728e7caa2512b6858
Doğukan Aydoğdu <daydogdu93@gmail.com>
2022-01-14 21:29:37 +0300

    v1.0.1 | get request parameters as function param

--- requester.go
+++ requester.go
@@ -10,53 +10,56 @@ import (
 // Requester represent the package structure, with creating exactly the same interface your own codebase you can
 // easily mock the functions inside this package while writing unit tests.
 type Requester interface {
-	Get() (*http.Response, error)
-	Post() (*http.Response, error)
-	Put() (*http.Response, error)
-	Delete() (*http.Response, error)
+	Get(re RequestEntity) (*http.Response, error)
+	Post(re RequestEntity) (*http.Response, error)
+	Put(re RequestEntity) (*http.Response, error)
+	Delete(re RequestEntity) (*http.Response, error)
 }
 
-type Request struct {
+// RequestEntity contains required information for sending http request
+type RequestEntity struct {
 	Timeout  int
 	Headers  []map[string]interface{}
 	Endpoint string
 	Body     []byte
 }
 
+type Request struct{}
+
 // Get simply send get http request to the given endpoint and return *http.Response and error if have it
-func (r *Request) Get() (*http.Response, error) {
-	return r.sendRequest(http.MethodGet)
+func (r *Request) Get(re RequestEntity) (*http.Response, error) {
+	return r.sendRequest(http.MethodGet, re)
 }
 
 // Post simply send post http request to the given endpoint and return *http.Response and error if have it
-func (r *Request) Post() (*http.Response, error) {
-	return r.sendRequest(http.MethodPost)
+func (r *Request) Post(re RequestEntity) (*http.Response, error) {
+	return r.sendRequest(http.MethodPost, re)
 }
 
 // Put simply execute put http request to the given endpoint and return *http.Response and error if have it
-func (r *Request) Put() (*http.Response, error) {
-	return r.sendRequest(http.MethodPut)
+func (r *Request) Put(re RequestEntity) (*http.Response, error) {
+	return r.sendRequest(http.MethodPut, re)
 }
 
 // Delete send delete method
-func (r *Request) Delete() (*http.Response, error) {
-	return r.sendRequest(http.MethodDelete)
+func (r *Request) Delete(re RequestEntity) (*http.Response, error) {
+	return r.sendRequest(http.MethodDelete, re)
 }
 
-func (r *Request) sendRequest(httpMethod string) (*http.Response, error) {
-	req, err := http.NewRequest(httpMethod, r.Endpoint, r.readBody())
+func (r *Request) sendRequest(httpMethod string, re RequestEntity) (*http.Response, error) {
+	req, err := http.NewRequest(httpMethod, re.Endpoint, re.readBody())
 
 	if err != nil {
 		return nil, err
 	}
 
 	req.Close = true
-	r.applyHeadersToRequest(req)
+	re.applyHeadersToRequest(req)
 
-	return (&http.Client{Timeout: r.applyTimeout()}).Do(req)
+	return (&http.Client{Timeout: re.applyTimeout()}).Do(req)
 }
 
-func (r *Request) applyHeadersToRequest(request *http.Request) {
+func (r *RequestEntity) applyHeadersToRequest(request *http.Request) {
 	for _, header := range r.Headers {
 		for key, value := range header {
 			if key == "Host" {
@@ -68,7 +71,7 @@ func (r *Request) applyHeadersToRequest(request *http.Request) {
 	}
 }
 
-func (r *Request) applyTimeout() time.Duration {
+func (r *RequestEntity) applyTimeout() time.Duration {
 	if r.Timeout <= 0 {
 		r.Timeout = 30
 
@@ -78,6 +81,6 @@ func (r *Request) applyTimeout() time.Duration {
 	return time.Duration(r.Timeout) * time.Second
 }
 
-func (r *Request) readBody() *strings.Reader {
+func (r *RequestEntity) readBody() *strings.Reader {
 	return strings.NewReader(string(r.Body))
 }
